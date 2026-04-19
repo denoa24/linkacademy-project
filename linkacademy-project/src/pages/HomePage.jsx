36a752a2d9304cf4fbe55ba66f7243b1e1a8e4f4
denoa24 <denis.vna24@gmail.com>
2026-04-19 18:46:44 +0300

    cart din backend si updatat display quantity

--- linkacademy-project/src/pages/HomePage.jsx
+++ linkacademy-project/src/pages/HomePage.jsx
@@ -6,10 +6,15 @@ import './HomePage.css';
 
 export function HomePage() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products').then((response) => {
             setProducts(response.data);
+        });
+
+        axios.get('http://localhost:3000/api/cart-items').then((response) => {
+            setCart(response.data);
         });
     }, []);
     
@@ -19,7 +24,7 @@ export function HomePage() {
             <title>Linkacademy Project</title>
             <link rel="icon" type="image/svg+xml" href="homeicon.svg" />
 
-            <Header />
+            <Header cart={cart}/>
 
             <div className="home-page">
                 <div className="products-grid">
