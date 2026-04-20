fa6f61c2ab17accc9f6c658dfd74b65e927f731f
denoa24 <denis.vna24@gmail.com>
2026-04-20 21:25:41 +0300

    facut functionale optiunile de livrare

--- linkacademy-project/src/pages/checkout/CheckoutPage.jsx
+++ linkacademy-project/src/pages/checkout/CheckoutPage.jsx
@@ -7,7 +7,7 @@ import './CheckoutHeader.css';
 import './CheckoutPage.css';
 
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -17,14 +17,13 @@ export function CheckoutPage({ cart }) {
       let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
       setDeliveryOptions(response.data);
 
-
       response = await axios.get('/api/payment-summary')
       setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
 
-  }, []);
+  }, [cart]);
 
   return (
     <>
@@ -37,7 +36,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
 
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
