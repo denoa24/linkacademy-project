0448daa24b090d2f6f0306bf261dd735b0015910
denoa24 <denis.vna24@gmail.com>
2026-04-22 19:50:08 +0300

    am convertit money.js in .ts

--- linkacademy-project/src/components/Header.tsx
+++ linkacademy-project/src/components/Header.tsx
@@ -5,7 +5,15 @@ import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './Header.css';
 
-export function Header({ cart }) {
+type HeaderProps = {
+    cart: {
+        productId: string;
+        quantity: number;
+        deliveryOptionId: string;
+    }[];
+}
+
+export function Header({ cart }: HeaderProps) {
     let totalQuantity = 0;
 
     cart.forEach((cartItem) => {
